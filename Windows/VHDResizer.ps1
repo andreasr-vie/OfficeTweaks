64e779bcb238827c7c0d3236ba5c061504a79bd5
Andreas R <114869961+andreasr-vie@users.noreply.github.com>
2022-10-21 12:22:37 +0200

    Create VHDResizer.ps1

---
+++ Windows/VHDResizer.ps1
@@ -0,0 +1,25 @@
+﻿$path = "D:\tmp\vhdtest\*.vhdx"
+$files = Get-ChildItem -File $path
+[long]$newSize = Read-Host -Prompt "Neue Disksize in GB eingeben: "
+[long]$newSize = $newSize * 1073741824
+$partitionNumber = Read-Host -Prompt "Partition-ID eingeben (bei UPD 1): "
+if ([string]::IsNullOrWhiteSpace($partitionNumber)) { $partitionNumber = 1 }
+
+#########################################
+
+Write-Host $newSize
+
+$files | ForEach-Object {Get-VHD -path $_.FullName -ErrorAction Ignore} | Sort-Object -Property Size | Format-Table -AutoSize -Property Path, @{Name="GB"; Expression={[math]::round($_.Size/1GB, 2)}}
+$files | ForEach-Object {Resize-VHD -Path $_.FullName -SizeBytes $newSize}
+
+foreach ($file in $files)
+{
+
+    Write-Host("Das ist Disk Nummer " + $file.FullName)
+    $size = (Mount-VHD -Path $file.FullName -PassThru | Get-Disk | Get-PartitionSupportedSize -PartitionNumber $partitionNumber)
+    Write-Host("Size: " + $size)
+    Dismount-VHD -Path $file.FullName
+    Mount-VHD -Path $file.FullName -PassThru | Get-Disk | Resize-Partition -PartitionNumber $partitionNumber -Size $size.SizeMax
+    Dismount-VHD -Path $file.FullName
+
+}
